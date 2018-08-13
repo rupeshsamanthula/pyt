6d08aa9333ea3dc108c895cf65218fe9a5126fdf
rupeshsamanthula <41769979+rupeshsamanthula@users.noreply.github.com>
2018-08-13 14:50:43 +0530

    Update vowcon.py

--- vowcon.py
+++ vowcon.py
@@ -1,10 +1,10 @@
 ch=input("")
 if((ch>='a' or ch<='z') and (ch>='A' or ch>='Z')):
 	if(ch=='a',ch=='e',ch=='i',ch=='o',ch=='u',ch=='A',ch=='E',ch=='I',ch=='O',ch=='U'):
-		print('vowel',end="")
+		print('Vowel',end="")
 		
 	else:
-		print('constant',end="")
+		print('Constant',end="")
 else:
 	print('notvalid',end="")
 	
