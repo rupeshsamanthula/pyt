6ed39653e2345edc8b8a5a8b5de4cccfd01e7e67
rupeshsamanthula <41769979+rupeshsamanthula@users.noreply.github.com>
2018-08-16 15:40:32 +0530

    Create primeintwo.py

---
+++ primeintwo.py
@@ -0,0 +1,20 @@
+n,k=input().split()
+n=int(n)
+k=int(k)
+count=0
+for i in range(n,k+1):
+	tem=i
+	b=0
+	#for j in range(0,i):
+	while(temp!=0):
+		a=temp%10
+		b+=a**3
+		temp=temp//10
+	if(i==b):
+		count+=1
+		if(count==1):
+			k=''
+		else:
+			k=" "
+		print(end=k)
+		print(i,end="")
